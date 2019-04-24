739dbeaa9b0adf1c1ca9c4870b7a3a160da58efe
Baiketli Dijo <baiketlid@gmail.com>
2019-04-24 15:13:33 +0100

    - added the famous writers section which uses an accordion to display the writers, and displays two of the author's books on either side of the author using jQuery on the click of the author's name.

--- js/script.js
+++ js/script.js
@@ -13,5 +13,14 @@ $(document).ready(function () {
         } else {
             $('.gallery').removeClass('change');
         }
-    })
+    });
+
+    $('.writers-accordion').click(function (event) {
+        // console.log(event.target.id.split('-'));
+        if (event.target.id.split('-')[0] === 'button') {
+            // console.log('Hello');
+            $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+            $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
+    });
 });
